48071a65d6ce09facb8eecaccd410470e255fbc4
Matt Yoder <diapriid@gmail.com>
2014-12-11 09:14:26 -0600

    Travis experiment.

--- lib/sqed/boundary_finder/stage_finder.rb
+++ lib/sqed/boundary_finder/stage_finder.rb
@@ -44,7 +44,7 @@ class Sqed::BoundaryFinder::StageFinder < Sqed::BoundaryFinder
     # works for 0.5, >0.137; 0.60, >0.14 0.65, >0.146; 0.70, >0.1875; 0.75, >0.1875; 0.8, >0.237; 0.85, >0.24; 0.90, >0.28; 0.95, >0.25
     # fails for 0.75, (0.18, 0.17,0.16,0.15); 0.70, 0.18;
     # fuzz = (2**16 * fuzz_factor).to_i  #same fuzz? not really, according to object_id
-    fuzz = (2**QuantumDepth * fuzz_factor).to_i  # could use QuantumRange instead of 2**Qu..
+    fuzz = (2**::QuantumDepth * fuzz_factor).to_i  # could use QuantumRange instead of 2**Qu..
 
     # Returns true if the edge is a border. (?) (border meaning outer region to be cropped)
     lambda do |edge|
