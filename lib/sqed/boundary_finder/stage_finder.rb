9aaa2aeb3e87743aef90f1d47f375ef6db4dcea0
Matt Yoder <diapriid@gmail.com>
2014-12-16 10:16:07 -0600

    Removes QuantumDepth for ::QuantumRange, which is not supported on some platforms.

--- lib/sqed/boundary_finder/stage_finder.rb
+++ lib/sqed/boundary_finder/stage_finder.rb
@@ -44,7 +44,7 @@ class Sqed::BoundaryFinder::StageFinder < Sqed::BoundaryFinder
     # works for 0.5, >0.137; 0.60, >0.14 0.65, >0.146; 0.70, >0.1875; 0.75, >0.1875; 0.8, >0.237; 0.85, >0.24; 0.90, >0.28; 0.95, >0.25
     # fails for 0.75, (0.18, 0.17,0.16,0.15); 0.70, 0.18;
     # fuzz = (2**16 * fuzz_factor).to_i  #same fuzz? not really, according to object_id
-    fuzz = (2**::QuantumDepth * fuzz_factor).to_i  # could use QuantumRange instead of 2**Qu..
+    fuzz = ((::QuantumRange + 1)  * fuzz_factor).to_i  # could use QuantumRange instead of 2**Qu..
 
     # Returns true if the edge is a border. (?) (border meaning outer region to be cropped)
     lambda do |edge|
@@ -67,6 +67,7 @@ class Sqed::BoundaryFinder::StageFinder < Sqed::BoundaryFinder
       border.to_f / (border + non_border) > threshold # number of matching string of pixels/(2 x total pixels - a.k.a. samples?)
     end
   end
+
   def vline(x)
     img.get_pixels x, @y00, 1, @height0 - 1
   end
@@ -74,7 +75,6 @@ class Sqed::BoundaryFinder::StageFinder < Sqed::BoundaryFinder
   def hline(y)
     img.get_pixels @x00, y, @width0 - 1, 1
   end
-
 
   private
 
