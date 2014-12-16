b311eacec6ecb9a711874eb54fb59a889fd9813d
Matt Yoder <diapriid@gmail.com>
2014-12-16 13:55:51 -0600

    Checkpoint in refactoring boundary finders. Moving most code to stage finder as it is unique to there.

--- lib/sqed/boundary_finder.rb
+++ lib/sqed/boundary_finder.rb
@@ -7,19 +7,15 @@ require 'RMagick'
 #
 class Sqed::BoundaryFinder
 
-  # How small we accept a cropped picture to be. E.G. if it was 100x100 and
   # ratio 0.1, min output should be 10x10
   MIN_BOUNDARY_RATIO = 0.01    
- 
+
   # How small we accept a cropped picture to be. E.G. if it was 100x100 and
   # ratio 0.1, min output should be 10x10
   MIN_CROP_RATIO = 0.1    # constant of this class
 
   # enumerate read-only parameters involved, accessible either as <varname> or @<varname>
   attr_reader :img, :x0, :y0, :x1, :y1, :min_width, :min_height, :rows, :columns 
-
-  # A Sqed::Boundaries instance, stores the coordinates of all of stage if present 
-  attr_reader :stage_boundary
 
   # A Sqed::Boundaries instance, stores the coordinates of all of the layout sections 
   attr_reader :boundaries
@@ -31,60 +27,19 @@ class Sqed::BoundaryFinder
   attr_reader :is_border
 
   # image must be supplied, others overridable
-  def initialize(image: image, is_border_proc: nil, min_ratio: MIN_CROP_RATIO, layout: layout, boundary_color: :green, stage_boundary: stage_boundary)
+  def initialize(image: image, is_border_proc: nil, min_ratio: MIN_CROP_RATIO, layout: layout, boundary_color: :green)
     @layout = layout
-    @stage_boundary = stage_boundary
-    raise 'No image provided.' if image.nil? || image.class != Magick::Image
-
-    # Initial co-ordinates
-    @img, @min_ratio = image, min_ratio
-    @x0, @y0 = 0, 0
-    @x1, @y1 = img.columns, img.rows 
-    @min_width, @min_height = img.columns * @min_ratio, img.rows * @min_ratio # minimum resultant area
-    @columns, @rows = img.columns, img.rows
-
-    # We need a border finder proc. Provide one if none was given.
-    @is_border = is_border_proc || self.class.default_border_finder(img)  # if no proc specified, use default below
-
-    true
-
-    # !! Each subclass should run find 
-  end
-  
-  # actually  + 1 (starting at zero?)
-  def width   
-    @x1 - @x0  
-  end
  
-  # actually  + 1 (starting at zero?)
-  def height
-    @y1 - @y0 
+    raise 'No image provided.' if image.nil? || image.class != Magick::Image
+    @img, @min_ratio = image, min_ratio
+    true
+    # !! Each subclass should run find 
   end
 
   # Returns a Sqed::Boundaries instance
   # defined in subclasses
   def boundaries
     @boundaries ||= Sqed::Boundaries.new()
-  end
-
-  # Returns a Proc that, given a set of pixels (an edge of the image) decides
-  # whether that edge is a border or not.
-  def self.default_border_finder(img, samples = 5, threshold = 0.75, fuzz = 0.20)   # initially 0.95, 0.05
-    # appears to assume sharp transition will occur in 5 pixels x/y
-    # how is threshold defined?
-    # works for 0.5, >0.137; 0.60, >0.14 0.65, >0.146; 0.70, >0.1875; 0.75, >0.1875; 0.8, >0.237; 0.85, >0.24; 0.90, >0.28; 0.95, >0.25
-    # fails for 0.75, (0.18, 0.17,0.16,0.15); 0.70, 0.18;
-    fuzz = (2**16 * fuzz).to_i  #same fuzz? not really, according to object_id
-
-    # Returns true if the edge is a border. (?)
-    lambda do |edge|
-      border, non_border = 0.0, 0.0
-
-      pixels = (0...samples).map { |n| edge[n * edge.length / samples] }
-      pixels.combination(2).each { |a, b| a.fcmp(b, fuzz) ? border += 1 : non_border += 1 }
-
-      border.to_f / (border + non_border) > threshold
-    end
   end
 
   # Returns: the column (x position) in the middle of the single green vertical line dividing the stage
@@ -122,7 +77,7 @@ class Sqed::BoundaryFinder
           # we have already hit that column previously, increment
           if border_hits[index]
             border_hits[index] += 1
-          # initialize the newly hit column 1 
+            # initialize the newly hit column 1 
           else
             border_hits[index] = 1
           end
@@ -131,7 +86,7 @@ class Sqed::BoundaryFinder
     end
 
     return nil if border_hits.length < 2
-    
+
     if sample_cutoff_factor.nil?
       cutoff = max_difference(border_hits.values)
     else
@@ -202,32 +157,4 @@ class Sqed::BoundaryFinder
 
   private
 
-  def find_edges
-    return unless is_border
-
-    u = x1 - 1
-    x0.upto(u)     { |x| width_croppable?  && is_border[vline(x)] ? @x0 = x + 1 : break }
-    (u).downto(x0) { |x| width_croppable?  && is_border[vline(x)] ? @x1 = x - 1 : break }
-
-    u = y1 - 1
-    0.upto(u)      { |y| height_croppable? && is_border[hline y] ? @y0 = y + 1 : break }
-    (u).downto(y0) { |y| height_croppable? && is_border[hline y] ? @y1 = y - 1 : break }
-    u = 0
-  end
-
-  def vline(x)
-    img.get_pixels x, y0, 1, height - 1
-  end
-
-  def hline(y)
-    img.get_pixels x0, y, width - 1, 1
-  end
-
-  def width_croppable?
-    width > min_width
-  end
-
-  def height_croppable?
-    height > min_height
-  end
 end
--- lib/sqed/boundary_finder/stage_finder.rb
+++ lib/sqed/boundary_finder/stage_finder.rb
@@ -13,7 +13,18 @@ class Sqed::BoundaryFinder::StageFinder < Sqed::BoundaryFinder
 
   def initialize(image: image, is_border_proc: nil, min_ratio: MIN_CROP_RATIO, layout: layout, boundary_color: :green)
     @layout = :internal_box
+
     super
+
+    # Initial co-ordinates
+    @x0, @y0 = 0, 0
+    @x1, @y1 = img.columns, img.rows 
+    @min_width, @min_height = img.columns * @min_ratio, img.rows * @min_ratio # minimum resultant area
+    @columns, @rows = img.columns, img.rows
+
+    # We need a border finder proc. Provide one if none was given.
+    @is_border = is_border_proc || self.class.default_border_finder(img)  # if no proc specified, use default below
+
     @x00 = @x0
     @y00 = @y0
     @height0 = height
@@ -21,6 +32,36 @@ class Sqed::BoundaryFinder::StageFinder < Sqed::BoundaryFinder
     find_edges
     xxx = 0
     # output
+  end
+
+  # actually  + 1 (starting at zero?)
+  def width   
+    @x1 - @x0  
+  end
+ 
+  # actually  + 1 (starting at zero?)
+  def height
+    @y1 - @y0 
+  end
+
+  # Returns a Proc that, given a set of pixels (an edge of the image) decides
+  # whether that edge is a border or not.
+  def self.default_border_finder(img, samples = 5, threshold = 0.75, fuzz = 0.20)   # initially 0.95, 0.05
+    # appears to assume sharp transition will occur in 5 pixels x/y
+    # how is threshold defined?
+    # works for 0.5, >0.137; 0.60, >0.14 0.65, >0.146; 0.70, >0.1875; 0.75, >0.1875; 0.8, >0.237; 0.85, >0.24; 0.90, >0.28; 0.95, >0.25
+    # fails for 0.75, (0.18, 0.17,0.16,0.15); 0.70, 0.18;
+    fuzz = (2**16 * fuzz).to_i  #same fuzz? not really, according to object_id
+
+    # Returns true if the edge is a border. (?)
+    lambda do |edge|
+      border, non_border = 0.0, 0.0
+
+      pixels = (0...samples).map { |n| edge[n * edge.length / samples] }
+      pixels.combination(2).each { |a, b| a.fcmp(b, fuzz) ? border += 1 : non_border += 1 }
+
+      border.to_f / (border + non_border) > threshold
+    end
   end
 
   def output
@@ -114,5 +155,36 @@ class Sqed::BoundaryFinder::StageFinder < Sqed::BoundaryFinder
     boundaries.complete = true 
     boundaries.coordinates[0] = [x0 + delta_x, y0 + delta_y, width - 2*delta_x, height - 2*delta_y]
   end
+
+# Superclass version
+# def find_edges
+#     return unless is_border
+
+#     u = x1 - 1
+#     x0.upto(u)     { |x| width_croppable?  && is_border[vline(x)] ? @x0 = x + 1 : break }
+#     (u).downto(x0) { |x| width_croppable?  && is_border[vline(x)] ? @x1 = x - 1 : break }
+
+#     u = y1 - 1
+#     0.upto(u)      { |y| height_croppable? && is_border[hline y] ? @y0 = y + 1 : break }
+#     (u).downto(y0) { |y| height_croppable? && is_border[hline y] ? @y1 = y - 1 : break }
+#     u = 0
+#   end
+
+  def vline(x)
+    img.get_pixels x, y0, 1, height - 1
+  end
+
+  def hline(y)
+    img.get_pixels x0, y, width - 1, 1
+  end
+
+  def width_croppable?
+    width > min_width
+  end
+
+  def height_croppable?
+    height > min_height
+  end
+
 
 end
